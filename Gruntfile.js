ca277dcefaa847795af3408d6427dd762b3c02d8
Pasi Lampinen <plam@fl027.local>
2017-08-25 10:18:28 +0300

    Add older browser support for css autoprefixer

--- Gruntfile.js
+++ Gruntfile.js
@@ -148,7 +148,7 @@ module.exports = function (grunt) {
     // Add vendor prefixed styles
     autoprefixer: {
       options: {
-        browsers: ['last 1 version']
+        browsers: ['last 40 versions']
       },
       dist: {
         files: [{
