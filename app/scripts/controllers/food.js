608ffe6f5a7fe25d4879b714984fb9f473c93b0f
Pasi Lampinen <plam@fl027.local>
2017-08-25 10:21:12 +0300

    Lunch fetching

--- app/scripts/controllers/food.js
+++ app/scripts/controllers/food.js
@@ -7,47 +7,74 @@
  * # FoodCtrl
  * Get food data
  */
+ var RESTAURANT_LIMIT = 9;
+ var APPROVED_RESTAURANTS = [
+   '/ravintola-tampella',
+   '/sodexo-tampereen-oikeustalo',
+   '/trattoria',
+   '/valssi',
+   '/speakeasy-finlayson-ylkerta', //-ylkerta and -alakertoa
+   '/fabric',
+   '/juvenes-frenckell',
+   '/the-grill',
+   '/ohranjyva',
+ ];
+
 angular.module('futudashApp')
-  .controller('FoodCtrl', function ($scope,$sce,Food) {
+  .controller('FoodCtrl', function ($scope,$sce,Food, $interval) {
 
     $scope.foodLoading = true;
+    var loadFoodInterval = 60 * 60 * 1000;
 
-		Food.get()
-		.then(function(data) {
+    var loadFood = function() {
+  		Food.get()
+  		.then(function(data) {
 
-      var limit = 9;
-
-      var el = document.createElement( 'div' );
-      el.innerHTML = data;
-      var sections = el.getElementsByClassName( 'menu' );
-
-      var foodData = [].slice.call( sections ); // HTMLCollection to Array
-      foodData = foodData.slice(0, limit);
+        var el = document.createElement( 'div' );
+        el.innerHTML = data;
+        var sections = el.getElementsByClassName( 'menu' );
 
 
-			// var el = document.createElement( 'div' );
-			// el.innerHTML = data;
-			// var sections = el.getElementsByClassName( 'menu' );
+        var foodData = [].slice.call( sections ); // HTMLCollection to Array
 
-      //    var sections;
-			// // // filter ones without menu (have class 'missing')
-			// sections = _.filter(sections,function(section){
-			// 	return section.innerHTML.indexOf('missing') < 0
-			// })
+        // # Filter approved restaurants
+        foodData = _.filter(foodData, function(place) {
+          return _.findIndex(APPROVED_RESTAURANTS, function(restaurant) {
+            return place.innerHTML.indexOf(restaurant) >= 0;
+          }) >= 0;
+        });
+
+        // max limit
+        foodData = foodData.slice(0, RESTAURANT_LIMIT);
 
 
-			// convert to string
-			var html = _.map(foodData, function(node){
-	        return node.outerHTML || "";
-	    }).join("");
+  			// var el = document.createElement( 'div' );
+  			// el.innerHTML = data;
+  			// var sections = el.getElementsByClassName( 'menu' );
 
-		  $scope.foodData  = $sce.trustAsHtml(html);
-      $scope.foodLoading = false;
+        //    var sections;
+  			// // // filter ones without menu (have class 'missing')
+  			// sections = _.filter(sections,function(section){
+  			// 	return section.innerHTML.indexOf('missing') < 0
+  			// })
 
-		}, function(data) {
-		  console.log('No food data for you');
-      $scope.foodLoading = false;
-		});
+
+  			// convert to string
+  			var html = _.map(foodData, function(node){
+  	        return node.outerHTML || '';
+  	    }).join('');
+
+  		  $scope.foodData  = $sce.trustAsHtml(html);
+        $scope.foodLoading = false;
+
+  		}, function(err) {
+  		  console.log('No food data for you', err);
+        $scope.foodLoading = false;
+  		});
+    };
+
+    $interval(loadFood, loadFoodInterval);
+    loadFood();
 
 
   });
