6c9e575f4a4688c284e345f8bb115927219ae8de
Pasi Lampinen <plam@fl027.local>
2017-08-25 10:19:41 +0300

    movie showing

--- app/scripts/controllers/movie.js
+++ app/scripts/controllers/movie.js
@@ -9,38 +9,45 @@
  */
 angular.module('futudashApp')
   .controller('MovieCtrl', function ($scope,$sce,Movies,Settings) {
-    
   	$scope.title = Settings.get().finnkinoName;
 
 		$scope.movies = 'Loading movies...'
-		// MOVIES 
+		// MOVIES
 		Movies.get(Settings.get().finnkinoArea)
-		.then(function(data){
+		.then(function(data) {
 
 		    var jsonData = x2js.xml_str2json(data);
 
 		    // group by movie
-		    $scope.movies = _.groupBy(jsonData.Schedule.Shows_asArray[0].Show_asArray,function(movie){ return movie.Title });
+		    $scope.movies = _.groupBy(jsonData.Schedule.Shows_asArray[0].Show_asArray,
+          function(movie) {
+            return movie.Title;
+          });
 
-		    $scope.movies = _.filter($scope.movies, function(movie){ return $scope.filterStartedMovies(movie)});
-		    console.log($scope.movies)
-		});    
+		    $scope.movies = _.filter($scope.movies,
+          function(movie){
+            return $scope.filterStartedMovies(movie);
+          });
+
+		    // console.log($scope.movies);
+		});
 
 
 		// filter out already started showtimes
-		$scope.filterStartedShows = function(show){
-		   return moment(show.dttmShowStart).toDate() > new Date()
+    var today = moment();
+		$scope.filterStartedShows = function(show) {
+		  return moment(show.dttmShowStart).isAfter(today);
 		}
 
 		// filter out already started movies
-		$scope.filterStartedMovies = function(movie){
-		   return _.filter(movie, function(show){ return $scope.filterStartedShows(show)  }).length
+		$scope.filterStartedMovies = function(movie) {
+		  return _.filter(movie, function(show) {
+        return $scope.filterStartedShows(show);
+      }).length;
 		}
 
 		// movie times
 		$scope.getTime = function(time){
-		    return moment(time).format('HH:mm')
+		  return moment(time).format('HH:mm');
 		}
-
-
   });
